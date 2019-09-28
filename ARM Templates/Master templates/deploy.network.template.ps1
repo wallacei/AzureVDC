3798197e5dfb93b1bd915aa2a988bfa3000f57e2
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 21:52:10 +0100

    first commit

---
+++ ARM Templates/Master templates/deploy.network.template.ps1
@@ -0,0 +1,33 @@
+﻿param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ $deploymentName,
+
+ [string]
+ $templateFileName = "azuredeploy.network.json",
+
+ [string]
+ $templateParameterFileName = "azuredeploy.network.parameters.json"
+)
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#******************************************************************************
+$ErrorActionPreference = "Stop"
+$scriptDir = $PSScriptRoot + "\"
+
+# sign in
+Write-Host "Logging in...";
+Login-AzAccount;
+
+$params = @{
+	'Name' = $deploymentName
+    'Location' = 'NorthEurope'
+	'TemplateFile' = $scriptDir + $templateFileName
+    'TemplateParameterFile' = $scriptDir + $templateParameterFileName }
+	
+# Start the deployment
+Write-Host "Starting deployment...";
+#New-AzResourceGroupDeployment @params
+New-AzDeployment @params
