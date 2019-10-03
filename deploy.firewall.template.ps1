f64949ecd63ce8e34171e1f405c4b440ff451401
BuildTools <unconfigured@null.spigotmc.org>
2019-10-03 21:35:47 +0100

    Deploy firewall script

---
+++ deploy.firewall.template.ps1
@@ -0,0 +1,41 @@
+param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ $deploymentName,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $StorageAccountName,
+
+ [string]
+ $templateFileName = "azuredeploy.firewalls.json",
+
+ [string]
+ $templateParameterFileName = "azuredeploy.firewalls.parameters.json"
+)
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#******************************************************************************
+$ErrorActionPreference = "Stop"
+$scriptDir = $PSScriptRoot + "\"
+
+# Modify parameters to securestring
+$pfSenseStorageAccountName = ConvertTo-SecureString $StorageAccountName -AsPlainText -Force
+
+# sign in
+Write-Host "Logging in...";
+Login-AzAccount;
+
+$params = @{
+	'Name' = $deploymentName
+    'Location' = 'NorthEurope'
+    'pfSenseStorageAccountName' = $pfSenseStorageAccountName
+	'TemplateFile' = $scriptDir + $templateFileName
+    'TemplateParameterFile' = $scriptDir + $templateParameterFileName }
+	
+# Start the deployment
+Write-Host "Starting deployment...";
+#New-AzResourceGroupDeployment @params
+New-AzDeployment @params
