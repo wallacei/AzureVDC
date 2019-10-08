de5863fb9274c4f13014f3a6a431e3977dee6cd1
Ian Wallace <iwallace1975@gmail.com>
2019-10-08 18:56:10 +0100

    Additional templates to deploy UDRs

---
+++ deploy.routing.template.ps1
@@ -0,0 +1,33 @@
+﻿param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ $deploymentName,
+
+ [string]
+ $templateFileName = "azuredeploy.routing.json",
+
+ [string]
+ $templateParameterFileName = "azuredeploy.routing.parameters.json"
+)
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#******************************************************************************
+$ErrorActionPreference = "Stop"
+$scriptDir = $PSScriptRoot + "\" + "ARM Templates\Master templates\"
+
+# sign in
+Write-Host "Logging in...";
+Login-AzAccount;
+
+$params = @{
+	'Name' = $deploymentName
+    'Location' = 'NorthEurope'
+	'TemplateFile' = $scriptDir + $templateFileName
+    'TemplateParameterFile' = $scriptDir + $templateParameterFileName }
+	
+# Start the deployment
+Write-Host "Starting deployment...";
+#New-AzResourceGroupDeployment @params
+New-AzDeployment @params -Verbose
